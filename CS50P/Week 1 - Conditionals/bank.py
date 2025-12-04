823546ed92b6b846a272c8a0a6a13ac4d46eea19
Jamie <hoyer307@gmail.com>
2025-12-04 18:19:13 +0100

    fixed an issue

--- CS50P/Week 1 - Conditionals/bank.py
+++ CS50P/Week 1 - Conditionals/bank.py
@@ -1,13 +1,13 @@
 def greeting(text):
-    if text == "hello":
+    if text.startswith("hello"):
         return 0
-    elif text[0] == "h":
+    elif text.startswith("h"):
         return 20
     else:
         return 100
 
 def main():
-    output = greeting(input("Greeting: ").strip())
+    output = greeting(input("Greeting: ").strip().lower())
     print(f"${output}")
 
 if __name__ == "__main__":
