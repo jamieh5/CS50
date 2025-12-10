bfa9cc12040f3763fa32f1f20678683d82b9dc59
Jamie <hoyer307@gmail.com>
2025-12-10 13:10:39 +0100

    uploaded todays project

---
+++ CS50P/Week 2 - Loops/twttr.py
@@ -0,0 +1,15 @@
+def removeVowels(text):
+    vowels = ["A", "E", "I", "O", "U"]
+    output = ""
+    for c in text:
+        if c.upper() not in vowels:
+            output += c
+    return output
+
+
+def main():
+    text = input("Input: ")
+    print(removeVowels(text))
+
+if __name__ == "__main__":
+    main()
