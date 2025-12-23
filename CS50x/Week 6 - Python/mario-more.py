a8a8192472a3390e7c8f94a14897bb59da97cb1f
Jamie <hoyer307@gmail.com>
2025-12-23 14:31:27 +0100

    uploaded todays project

---
+++ CS50x/Week 6 - Python/mario-more.py
@@ -0,0 +1,19 @@
+# Problem set: https://cs50.harvard.edu/x/psets/6/mario/more/
+
+def main():
+    while True:
+        try:
+            height = int(input("Height: "))
+            if 1 <= height <= 8:
+                num_of_hashtags = 1
+                num_of_spaces = int(height) - 1
+
+                for i in range(0, height, 1):
+                    print(" " * num_of_spaces + "#" * num_of_hashtags + "  " + "#" * num_of_hashtags)
+                    num_of_hashtags += 1
+                    num_of_spaces -= 1
+                break
+        except ValueError:
+            print("Enter a number between 1 and 8")
+if __name__ == "__main__":
+    main()
