7b9aa2f182970c030c379a165ab5bd191f6e344e
Jamie <hoyer307@gmail.com>
2026-03-11 14:16:14 +0100

    query to look for hints in the crime scene report

--- CS50x/Week 7 - SQL/fiftyville/log.sql
+++ CS50x/Week 7 - SQL/fiftyville/log.sql
@@ -1,2 +1,5 @@
+-- Problem set: https://cs50.harvard.edu/x/psets/7/fiftyville/
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
+-- Getting the description from the crime reports when the crime was commited
+SELECT * FROM crime_scene_reports WHERE day = 28 AND month = 7 AND street = "Humphrey Street"
