d7aeb3951346a3d94fb9463e815f2414801248ed
Jamie <hoyer307@gmail.com>
2026-03-12 13:54:09 +0100

    Finished tasks 1 and 2.

--- CS50x/Week 7 - SQL/fiftyville/log.sql
+++ CS50x/Week 7 - SQL/fiftyville/log.sql
@@ -14,5 +14,23 @@ SELECT name, transcript FROM interviews WHERE day = 28 AND month = 7 AND transcr
         -- Witness Ruth: Within 10mins of the theft the thief got into a car and left
         -- Witness Eugene: Didnt know the thiefs name, but recognized him. The thief was withdrawing money from an ATM on Leggett Street
 
--- Looking for id for the fiftyville airport
-SELECT id FROM airports WHERE city = "Fiftyville" -- ID = 8
+-- Intersection and joining the queries to find out the name of the thief
+SELECT people.name FROM people JOIN bakery_security_logs ON people.license_plate = bakery_security_logs.license_plate WHERE day = 28 AND month = 7
+INTERSECT
+SELECT people.name FROM people JOIN bank_accounts ON people.id = bank_accounts.person_id JOIN atm_transactions ON atm_transactions.account_number = bank_accounts.account_number WHERE day = 28 AND month = 7 AND atm_location = "Leggett Street" AND transaction_type = "withdraw"
+INTERSECT
+SELECT people.name FROM people JOIN phone_calls ON people.phone_number = phone_calls.caller WHERE day = 28 AND month = 7 AND duration < 60
+INTERSECT
+SELECT people.name FROM people JOIN passengers ON people.passport_number = passengers.passport_number WHERE passengers.flight_id IS (
+    SELECT id FROM flights WHERE day = 29 AND month = 7
+)
+-- The name of the thief is Diana
+
+-- Getting destination airport_id for Diana´s flight
+SELECT destination_airport_id FROM flights JOIN passengers ON passengers.flight_id = flights.id WHERE day = 29 AND month = 7 AND passengers.passport_number IS (
+    SELECT passport_number FROM people WHERE name = "Diana"
+)
+
+-- Getting Airport name for the airport with ID 6
+SELECT city FROM airports WHERE id = 6
+-- Diana fled to Boston
