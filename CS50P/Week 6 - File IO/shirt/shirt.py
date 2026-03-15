dcff9a016ba4c4f0b96ca1db0aca915c339caa2e
Jamie <hoyer307@gmail.com>
2026-03-15 11:54:11 +0100

    Finished the func for input validation

--- CS50P/Week 6 - File IO/shirt/shirt.py
+++ CS50P/Week 6 - File IO/shirt/shirt.py
@@ -1,13 +1,27 @@
 # Problem-set: https://cs50.harvard.edu/python/psets/6/shirt/
 import sys
+import os
+from PIL import Image
 
 def main():
     input_validating()
 
-
 def input_validating():
-    endings = ["jpeg", "jpg", "png"]
-    input, output = sys.argv[0], sys.argv[1]
+    if len(sys.argv) > 3:
+        sys.exit("Too many command-line arguments")
+
+    if len(sys.argv) < 3:
+        sys.exit("Too few command-line arguments")
+
+    endings = [".jpeg", ".jpg", ".png"]
+    ext1 = os.path.splitext(sys.argv[1])[1]
+    ext2 = os.path.splitext(sys.argv[2])[1]
+    if ext1 != ext2 or ext1 not in endings:
+        sys.exit("Invalid input")
+
+
+def overlap_images():
+    ...
 
 
 if __name__ == "__main__":
