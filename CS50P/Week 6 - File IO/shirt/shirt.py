2eb7f2ceb58b10e75a36753c6b82b222d081df5a
Jamie <hoyer307@gmail.com>
2026-03-15 09:24:23 +0100

    Uploaded the boilerplate code

---
+++ CS50P/Week 6 - File IO/shirt/shirt.py
@@ -0,0 +1,14 @@
+# Problem-set: https://cs50.harvard.edu/python/psets/6/shirt/
+import sys
+
+def main():
+    input_validating()
+
+
+def input_validating():
+    endings = ["jpeg", "jpg", "png"]
+    input, output = sys.argv[0], sys.argv[1]
+
+
+if __name__ == "__main__":
+    main()
