45b206ca986b4f00f1063598aec527141b333a1c
Jamie <hoyer307@gmail.com>
2026-03-22 10:54:53 +0100

    Finished the trivia project.

--- CS50x/Week 8 - HTML, CSS, JavaScript/trivia/script.js
+++ CS50x/Week 8 - HTML, CSS, JavaScript/trivia/script.js
@@ -1,6 +1,7 @@
 let choices_btn = document.querySelectorAll(".choice");
 let choices_p = document.querySelectorAll(".choices p");
 
+// Handling the correct answer for the first question
 for (let i = 0; i < choices_btn.length; i++) {
   choices_btn[i].addEventListener("click", function () {
     choices_p[i].style.visibility = "visible";
@@ -11,3 +12,18 @@ for (let i = 0; i < choices_btn.length; i++) {
     }
   });
 }
+
+// Handling the correct answer for the second question
+document.querySelector("form").addEventListener("submit", function (event) {
+  event.preventDefault();
+  answer = document.querySelector("#answer");
+  p = document.querySelector("#form-p");
+  if (answer.value != 24) {
+    p.innerHTML = "Incorrect!";
+    answer.style.backgroundColor = "red";
+  } else {
+    p.innerHTML = "Correct!";
+    answer.style.backgroundColor = "green";
+  }
+  p.style.visibility = "visible";
+});
