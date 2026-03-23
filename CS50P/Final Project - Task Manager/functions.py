89b1750e2aadd02e649ad85d89c32c227490f678
Jamie <hoyer307@gmail.com>
2026-03-23 15:08:46 +0100

    Added the Task class

--- CS50P/Final Project - Task Manager/functions.py
+++ CS50P/Final Project - Task Manager/functions.py
@@ -1,3 +1,31 @@
+import csv
+from datetime import datetime
+
+class Task():
+    def __init__(self, title, priority, status="Incomplete", id=None, due_date=None):
+        if not title.strip():
+            raise ValueError("No title")
+        if priority not in ["low", "medium", "high"]:
+            raise ValueError("Wrong priority set")
+        if due_date:
+            try:
+                datetime.strptime(due_date, "%Y-%m-%d")
+            except ValueError:
+                raise ValueError("Invalid date format, use YYYY-MM-DD")
+
+        self.id = id
+        self.title = title
+        self.due_date = due_date
+        self.priority = priority
+        self.status = status
+        self.created_at = datetime.now().strftime("%Y-%m-%d")
+
+def get_task():
+    title = input("Title: ")
+    priority = input("Priority: ")
+    due_date = input("Due date: ")
+    return Task(title=title, priority=priority, due_date=due_date)
+
 def add_tasks():
     ...
 
@@ -11,4 +39,4 @@ def completed():
     ...
 
 def list_tasks():
-    ...
+   ...
--- CS50P/Final Project - Task Manager/main.py
+++ CS50P/Final Project - Task Manager/main.py
@@ -1,7 +1,9 @@
 import functions
 
 def main():
-    ...
+    task1 = functions.get_task()
+    print(task1)
+
 
 if __name__ == "__main__":
     main()
