4b871148c933e9acf0ab72d6b2c412a162b4fe42
Jamie <hoyer307@gmail.com>
2026-05-15 12:12:36 +0200

    Added some unit tests for the player function.

---
+++ CS50AI/Week 0 - Search/TicTacToe/testing_boards.py
@@ -0,0 +1,21 @@
+# File where i store all the tictactoe boards for testing
+
+board1 = [[None, None, None],
+         [None, None, None],
+         [None, None, None]]
+
+board2 = [[None, None, None],
+         [None, "X", None],
+         [None, None, None]]
+
+board3 = [[None, None, None],
+         [None, "X", None],
+         [None, None, "O"]]
+
+board4 = [[None, "X", None],
+         [None, "X", None],
+         [None, None, "O"]]
+
+board5 = [[None, "X", None],
+         [None, "X", None],
+         [None, "O", "O"]]
